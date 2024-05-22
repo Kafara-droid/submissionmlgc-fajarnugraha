dc281c260613eeed14305a9a903b4fa732c58340
Kafara-droid <fajar.nugraha_ti20@nusaputra.ac.id>
2024-05-22 20:06:22 +0700

    update docker

--- src/server/server.js
+++ src/server/server.js
@@ -1,37 +1,38 @@
 require('dotenv').config();
-
+ 
 const Hapi = require('@hapi/hapi');
 const routes = require('../server/routes');
 const loadModel = require('../services/loadModel');
 const InputError = require('../exceptions/InputError');
-
+ 
 (async () => {
     const server = Hapi.server({
-        port: 3000,
-        // mendefinisikan host
-        host: 'localhost',
+        port: process.env.PORT,
+        host: '0.0.0.0',
         routes: {
             cors: {
-                origin: ['*'],
+              origin: ['*'],
             },
         },
     });
-
+ 
     const model = await loadModel();
     server.app.model = model;
-
+ 
     server.route(routes);
+ 
     server.ext('onPreResponse', function (request, h) {
         const response = request.response;
+ 
         if (response instanceof InputError) {
             const newResponse = h.response({
                 status: 'fail',
-                message: 'Terjadi kesalahan dalam melakukan prediksi'
+                message: `Terjadi kesalahan dalam melakukan prediksi`
             })
             newResponse.code(response.statusCode)
             return newResponse;
         }
-
+ 
         if (response.isBoom) {
             const newResponse = h.response({
                 status: 'fail',
@@ -40,7 +41,7 @@ const InputError = require('../exceptions/InputError');
             newResponse.code(response.output.statusCode)
             return newResponse;
         }
-
+ 
         return h.continue;
     });
  
